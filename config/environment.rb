ab6c4232caad3aeebb152a618925178f35252817
Micah Geisel <micah@botandrose.com>
2014-05-22 12:23:01 -0700

    simplify.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,17 @@
-# Load the Rails application.
-require File.expand_path('../application', __FILE__)
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module CucumberPoltergeistRubyTwoDotOneExitCode
+  class Application < Rails::Application
+    config.cache_classes = true
+    config.eager_load = false
+  end
+end
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: '_cucumber_poltergeist_ruby_two_dot_one_exit_code_session'
